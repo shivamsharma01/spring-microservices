23c8e9cbf2f8dbe284e29d3ee71d981b158623c2
Shivam Sharma <prisonmatch@gmail.com>
2020-03-06 17:28:11 +0530

    ribbon and feign client

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.in28minutes.microservices.currencyconversionservice.controller")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
